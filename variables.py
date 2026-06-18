4e3d6c7f47ff1c0b33d512135d15c287f02251d4
Precious Abangira <abangiraprecious@gmail.com>
2026-06-18 09:07:49 +0300

    Variables

--- variables.py
+++ variables.py
@@ -4,3 +4,8 @@ num1 = 100
 num2 = 200
 
 print(num1 + num2) #output: 300
+
+# variable names should not start with a number
+# variable names should not start with a capital letter
+# variable names should not start with any special character...#,&,/,@, etc...
+# a single variable name should not have spaces
