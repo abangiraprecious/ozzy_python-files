f1c0f6368e4cadfdc6b9dba0db0c5c7985980e16
Precious Abangira <abangiraprecious@gmail.com>
2026-06-18 09:56:07 +0300

    Data Types

--- data_types.py
+++ data_types.py
@@ -2,8 +2,18 @@
 
 # Data Types
 # 1. Numeric Data Types ( int,float,complex)
+# - int: 0-9
+# - float: decimal
 # 2. String (str)
+# - are in quotation marks
+# - all alphabetical words
 # 3. Sequence data type (list, tuple, range)
 # 4. Mapping (dict)
 # 5. Boolean (bool)
 # 6. Set (set, frozenset)
+
+
+# storing more than one value in a single variable - sequence data type
+
+languages = ["English", "Swahili", "Luganda", "Runyankore"] # - these can take on more than one data type
+# - a list can take on another list
