3a6086cc52748cdf72eb47aad79164c125414b02
Precious Abangira <abangiraprecious@gmail.com>
2026-06-19 19:27:13 +0300

    Lists and Practice Files

--- Practice Files/lists.py
+++ Practice Files/lists.py
@@ -10,3 +10,13 @@ print(favourites[3])
 
 numbers = [1, 2, 3, 4, 5]
 print(len(numbers)) #print the len (length) of said numbers
+
+data = [1, 2, 3, [10, 20], 30]
+print(data[3]) #10,20
+print(data[3][0]) #10
+print(data[3][1])# 20
+
+mix = [100, [5, 10, 15], 200, [7, 8]]
+print(mix[1][2]) #15
+print(mix[-1][0])#7
+print(mix[1][-1])#15
