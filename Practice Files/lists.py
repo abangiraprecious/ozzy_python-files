361a57f8303997bd19057821aea7b704453e0c4e
Precious Abangira <abangiraprecious@gmail.com>
2026-06-19 19:24:20 +0300

    Lists and Practice Files

---
+++ Practice Files/lists.py
@@ -0,0 +1,12 @@
+#1. Your favourite things
+
+# Create a list called favourites with 4 of your favourite foods. Then print the first item and the last item.
+
+favourites = ["my boyfried", "coffee", "fruit tea", "books"]
+print(favourites[0])
+print(favourites[3])
+
+# Create a list of any 5 numbers. Print how many items are in it using a built-in Python function.
+
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers)) #print the len (length) of said numbers
