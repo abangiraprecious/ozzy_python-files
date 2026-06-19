200702d78bd55da9f81e607b2e4b7e32eb469552
Precious Abangira <abangiraprecious@gmail.com>
2026-06-19 10:55:33 +0300

    Indices

---
+++ tuple_type.py
@@ -0,0 +1,6 @@
+#tuple is like a list, it stores more than one value but it uses parentheses
+# a tuple stores things permanently. it cannot be manipulated
+
+names = ("angel", "donald", "martha")
+print(names[0])
+print(names[-1])
--- variables.py
+++ variables.py
@@ -11,8 +11,33 @@ print(num1 + num2) #output: 300
 # a single variable name should not have spaces
 # use relatable variable names
 
+# a list is mutable data type. you can add/remove
+
 fruits = ["mangoe", "apple", "dragonfruit"]
 print(fruits[0])
 print(fruits[2])
+print(fruits[-3])
+print(fruits.pop()) #pop removes the last element of the list
+fruits.append("papaya") #adds to the end 
 
-numbers = [1, 2, 3, [10, 20]] # the second list (inner) is in position 3. Rule of indices at work
+numbers = [1, 2, 3, [10, 20]] # the second list (inner) is in position 3. Rule of indices at work. To print 10 see below
+
+print(numbers[3][0])
+
+matrix = [[1,2,3], 
+          [4,5,6], 
+          [7,8,9]]
+
+#so if i want to print six...
+
+print(matrix[1][2])
+
+matrix_two = [1,2,3,[10,20],30,[40,[60,70]]]
+
+#to print 60
+
+print(matrix_two[5][1][0])
+
+# to print 70
+
+print(matrix_two[5][1][1])
