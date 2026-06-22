3afc108cab898c72fd91266c9a728ac8d1d0645f
Precious Abangira <abangiraprecious@gmail.com>
2026-06-22 09:10:25 +0300

    Basic Operators

---
+++ basic_operarators.py
@@ -0,0 +1,20 @@
+# + - / * %
+
+#addition
+a = 10
+b = 2
+print(a+b) #12
+
+#subtraction
+print (a-b)  #8
+
+#multiplication 
+print(a*b) #30
+
+print(a/b) #5 
+
+d = 7
+e = 2
+
+#modulus (remainder)
+print(d%e) #1
