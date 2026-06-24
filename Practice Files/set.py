a77fd5c0373deb00493c0f781bc85df946137fb1
Precious Abangira <abangiraprecious@gmail.com>
2026-06-24 16:07:37 +0300

    Set

--- Practice Files/set.py
+++ Practice Files/set.py
@@ -1,6 +1,8 @@
-# A list is ordered and changeable []. Ordered means that it remembers the exact postition of elmeents based on how you put them in.
-# - duplicates are okay within the list
-# - you can reassign
+# A set is unordered and immutable {} Unordereed means that it does track positioning in insertion sequence. So indices are out of the mix
+# - but you can add/remove
+# - no duplicates in that set. it ignores it
+
+
 #1. Your favourite things
 
 # Create a list called favourites with 4 of your favourite foods. Then print the first item and the last item.
@@ -8,31 +10,6 @@
 favourites = ["my boyfried", "coffee", "fruit tea", "books"]
 print(favourites[0])
 print(favourites[3])
-
-# Create a list of any 5 numbers. Print how many items are in it using a built-in Python function.
-
-numbers = [1, 2, 3, 4, 5]
-print(len(numbers)) #print the len (length) of said numbers
-
-data = [1, 2, 3, [10, 20], 30]
-print(data[3]) #10,20
-print(data[3][0]) #10
-print(data[3][1])# 20
-
-mix = [100, [5, 10, 15], 200, [7, 8]]
-print(mix[1][2]) #15
-print(mix[-1][0])#7
-print(mix[1][-1])#15
-
-m = [1, 2, 3, [10, 20], 30, [40, [60, 70]]]
-print(m[3][1]) #20
-print(m[5][0]) #40
-print(m[5][1]) #60,70
-
-m = [1, 2, 3, [10, 20], 30, [40, [60, 70]]]
-print(m[5][1][0])# 60
-print(m[5][1][1])# 70
-print(m[5][1][-1])# 70
 
 
 fruit = ["mangoe", "apple", "banana", "guava", "kiwi"]
