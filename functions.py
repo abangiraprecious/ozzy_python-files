420b050ca9d4705e50d71f7e6ad15ea727e8f4b2
Precious Abangira <abangiraprecious@gmail.com>
2026-06-26 08:33:02 +0300

    Functions

--- functions.py
+++ functions.py
@@ -2,3 +2,8 @@
 # a function in py is created with the use of the word "def" followed by a valid name of the function (must adhere to variable syntax)
 
 def stuff():
+    num1, num2 = 10, 20
+    print(num1 + num2)
+
+num1, num2 = 10, 20 # here it doesn't have an identity. They are independent statements that don't have any identity
+print(num1 + num2)
