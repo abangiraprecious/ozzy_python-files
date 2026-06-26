fa7f4da6cae22e70b31dcde3fc3f811d3fc70504
Precious Abangira <abangiraprecious@gmail.com>
2026-06-26 09:13:33 +0300

    function categories

--- function_categories.py
+++ function_categories.py
@@ -1,1 +1,10 @@
 # 1. User defined functions 
+# - functions the user creates (sw engineer)
+
+# 2. Inbuilt / predefined functions 
+# - inbuilt in python. All you have to do is call them. 
+#===================================|
+# print()                           |  
+# pop()                             |
+# append()                          |
+#these are inbuilt functions        |
