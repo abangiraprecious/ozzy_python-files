2750041c4f190dac2dc3f729be7bd79bbea28d77
Precious Abangira <abangiraprecious@gmail.com>
2026-06-26 08:26:29 +0300

    Functions

---
+++ functions.py
@@ -0,0 +1,4 @@
+# a function / routine is a named block of code that performs a specific task (one thing)
+# a function in py is created with the use of the word "def" followed by a valid name of the function (must adhere to variable syntax)
+
+def stuff():
