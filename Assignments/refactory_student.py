455f47d6ac0169e844db0680ba6509155e4ba9d4
Precious Abangira <abangiraprecious@gmail.com>
2026-06-27 13:29:05 +0300

    Refactory-Student

--- Assignments/refactory_student.py
+++ Assignments/refactory_student.py
@@ -3,25 +3,23 @@ age = int(input("Enter Your Full Age: "))
 location = input("Enter Your Location: ")
 phone_no = input("Enter Your Phone Number (0771234567): ")
 
+# I added this while loop to test my learning but also because it is common to put incorrect phone numbers. So I used the while loop to determine that it does not happen and, it is also not an infinite loop. 
 while len(phone_no) != 10 :
     print("Digits are not 10")
-    phone_no = input("Enter The Correct Phone Number (10 digits): ")
+    phone_no = input("Enter The Correct Phone Number (10 digits): ") 
 
-track = input("Enter Your Python Track (Python/Javascript): ")
+track = input("Enter Your CSE Track (Python/Javascript): ")
 
 intake = input("What intake are you in: ")
 
-
-#In an if-elif-else block, Python checks conditions top to bottom.
-# - The moment it finds the first True condition, it:mruns that block and then skips all the rest
 if age < 15:
-    print("You are not eligible because you are below 15 years")
+    print("Unfortunately, you are not eligible because you are below 15 years")
 
-elif track not in ("Python", "Javascript", "JavaScript"): #case sensitive
-    print("Not eligible for CSE because your track is invalid")
+elif track.lower() not in ["python", "javascript"]:
+    print("You are not eligible for CSE because your track is invalid.")
 
 else:
-    print(f"Dear {name}, you are eligible for CSE because you are above the age of 15 and your track is {track} which is part of CSE")
+    print(f"Dear {name}, you are eligible for CSE because you are above the age of 15 and your track is {track} which is part of CSE!")
 
 
 
