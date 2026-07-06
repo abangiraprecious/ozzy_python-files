4a1bfefc61e589dc730033b8bfca57454e2cc708
Precious Abangira <abangiraprecious@gmail.com>
2026-07-06 08:33:24 +0300

    Practice

---
+++ Functions/Practice/loops.py
@@ -0,0 +1,10 @@
+for i in range (1, 11):
+    print(i)
+
+for i in range (1, 21):
+    if i % 2 == 0:
+        print(i)
+
+names = ("Deen", "Joel", "Grace", "Musa")
+for name in names:
+    print(name)
---
+++ Python - WebDev/README.md
@@ -0,0 +1,1 @@
+Anacoda library
