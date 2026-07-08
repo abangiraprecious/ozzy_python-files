63e6cbb0312633463f917b69386a78a7c4cfe773
Precious Abangira <abangiraprecious@gmail.com>
2026-07-08 22:28:47 +0300

    inputs

---
+++ Practice_Files/input.py
@@ -0,0 +1,8 @@
+name = input("What is your name: ")
+print(f"Hello, {name}")
+
+age = int(input("Age: "))
+print(f"{name} is {age} years old")
+
+school = input("School: ")
+print(f"{name} is {age} years old and goes to {school} for school")
